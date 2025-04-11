48a7752c651dd0f176e18afc44db0f911454deff
Steven Rostedt (Google) <rostedt@goodmis.org>
2025-04-10 21:32:57 -0400

    libtracefs: Fix COMM for use as a field for tracefs_sqlhist()
    The COMM was added to be used as a filter, but it can also be used as a
    field. In order to do that, the type must be correct. The type should just
    be "char" and the array type should be a dynamic array.
    
    Link: https://lore.kernel.org/20250410213257.5e3550fd@gandalf.local.home
    Fixes: 9098ab0d ("libtracefs: Allow filters to use "COMM"")
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- src/tracefs-filter.c
+++ src/tracefs-filter.c
@@ -36,9 +36,10 @@ static const struct tep_format_field common_timestamp_usecs = {
 };
 
 static const struct tep_format_field common_comm = {
-	.type			= "char *",
+	.type			= "char",
 	.name			= "common_comm",
-	.size			= 16,
+	.size			= 4,
+	.flags			= TEP_FIELD_IS_ARRAY | TEP_FIELD_IS_DYNAMIC,
 };
 
 const struct tep_format_field tfs_common_stacktrace __hidden = {
